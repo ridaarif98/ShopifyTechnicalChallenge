62543969a842199d450f7dab4fcad430e0a13ce7
ridaarif98 <ridaarif16@gmail.com>
2022-06-03 16:45:31 +0200

    Define routes in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'items#index'
+  # get 'items/test'
+  resources :items, only: [:new, :create, :index, :update, :edit, :delete] do
+    resources :comments, only: [:create]
+  end
+  put 'items/:id/test', to: 'items#test', as: 'test'
+  get 'items/:id/show', to: 'items#show', as: 'show'
+  get 'items/deleted', to: 'items#deleted', as: 'deleted'
+  
+
 end
