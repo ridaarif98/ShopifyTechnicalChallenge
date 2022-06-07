598961e6bec5383c85e7a6d382fc216876630694
ridaarif98 <ridaarif16@gmail.com>
2022-06-07 21:09:03 +0200

    Setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,16 @@ Rails.application.routes.draw do
   # root "articles#index"
   root 'items#index'
   # get 'items/test'
-  resources :items, only: [:new, :create, :index, :update, :edit, :delete] do
+  resources :items, only: [:new, :create, :index, :update, :edit, :delete, :destroy] do
     resources :comments, only: [:create]
+    
   end
+  resources :comments, only: [:destroy]
   put 'items/:id/test', to: 'items#test', as: 'test'
+  put 'comments/:id/dell_comment', to: 'comments#dell_comment', as: 'dell_comment'
   get 'items/:id/show', to: 'items#show', as: 'show'
   get 'items/deleted', to: 'items#deleted', as: 'deleted'
+  get 'items/:item_id/comments/deleted_comments', to: 'comments#deleted_comments', as: 'deleted_comments'
   
 
 end
